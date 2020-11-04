b828c241c28551e7b75a01977df032e75ecdac65
muncheese <73927243+muncheese@users.noreply.github.com>
2020-11-04 12:13:30 +0200

    Add files via upload

---
+++ regCheck333.js
@@ -0,0 +1,38 @@
+
+
+
+
+const guestFirstName = "Anna";
+const guestLastName = "Berzina";
+const guestPhone = "12345678";
+const guestEmail = "anna@gmail.com";
+const confirmationMSG = document.getElementById("confirmationMSG");
+
+
+let firstname = document.getElementById("firstname").innerHTML;
+let lastname = document.getElementById("lastname").innerHTML;
+let phone = document.getElementById("phone").innerHTML;
+let email = document.getElementById("email").innerHTML;
+let message = document.getElementById("confirmationMSG").innerHTML;
+
+const regButton = document.getElementById("buttonID1");
+regButton.addEventListener("click", getGuestData);
+function getGuestData(e) {
+    e.preventDefault();
+    if (firstname.innerHTML === guestFirstName && lastname.innerHTML === guestLastName && phone.innerHTML === guestPhone && email.innerHTML === guestEmail) {
+        message.innerText = "Reģistrācija veiksmīga"
+    } else {
+        message.innerText = "Reģistrācija neizdevās. Lūdzu, pārbaudiet ievadīto informāciju"
+    }
+}
+
+regButton.addEventListener("blur", clearGuestData)
+
+function clearGuestData(e) {
+    e.preventDefault();
+    message.innerText = "";
+    firstname.innerHTML = "";
+    lastname.innerHTML = "";
+    phone.innerHTML = "";
+    email.innerHTML = "";
+}
